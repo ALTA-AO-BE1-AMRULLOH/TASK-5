aef92f6ddb8a39d67646ee726e94ff6216b06257
amrulloh <amrullmisri@gmail.com>
2023-07-16 13:38:15 +0700

    solve problem1

---
+++ problem1.js
@@ -0,0 +1,15 @@
+function exponentiation(x, n) {
+    //your code
+    const nilai = [2, 2, 7, 9, 22, 1996];
+       
+    return [x**n]
+}
+
+
+console.log(exponentiation(2, 3)); //8
+console.log(exponentiation(2, 12)); //4096
+console.log(exponentiation(7, 2)); //49
+console.log(exponentiation(9, 3)); //729
+console.log(exponentiation(22, 5)); //5153632
+console.log(exponentiation(1996, 0)); //1
+
